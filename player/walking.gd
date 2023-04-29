73d987c82c02b7e72fd73a054d586fc717a3cfa7
Andy Black <Sprotex@gmail.com>
2023-04-29 12:47:07 +0200

    add whole main level
    add proper keybindings
    add sky color
    split grid material in two
    fix movement along the camera
    refactor walking script
    add shadows
    add barriers

--- player/walking.gd
+++ player/walking.gd
@@ -1,31 +1,32 @@
 extends Node
 
 @export var body: CharacterBody3D
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var camera: Camera3D
+@export var speed := 5.0
+@export var jump_velocity := 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
-func _physics_process(delta):
-	# Add the gravity.
+func _handle_gravity(delta: float) -> void:
 	if not body.is_on_floor():
 		body.velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and body.is_on_floor():
-		body.velocity.y = JUMP_VELOCITY
+func _handle_jump(delta: float) -> void:
+	if Input.is_action_just_pressed("jump") and body.is_on_floor():
+		body.velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (body.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+func _physics_process(delta: float) -> void:
+	_handle_gravity(delta)
+	_handle_jump(delta)
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var right_direction = camera.transform.basis.x
+	var forward_direction = right_direction.cross(Vector3.UP)
+	var direction = (input_direction.x * right_direction + forward_direction * input_direction.y).normalized()
 	if direction:
-		body.velocity.x = direction.x * SPEED
-		body.velocity.z = direction.z * SPEED
+		body.velocity.x = direction.x * speed
+		body.velocity.z = direction.z * speed
 	else:
-		body.velocity.x = move_toward(body.velocity.x, 0, SPEED)
-		body.velocity.z = move_toward(body.velocity.z, 0, SPEED)
-
+		body.velocity.x = move_toward(body.velocity.x, 0, speed)
+		body.velocity.z = move_toward(body.velocity.z, 0, speed)
 	body.move_and_slide()
